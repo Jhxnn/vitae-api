005021d5e8b06c0f4add8c2dfdd2af38552a8530
Hilixx <jhonalizan@gmail.com>
2025-07-17 14:31:46 -0300

    first

---
+++ src/main/java/com/vitae_api/VitaeApiApplication.java
@@ -0,0 +1,13 @@
+package com.vitae_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VitaeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VitaeApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vitae_api/VitaeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vitae_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VitaeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
