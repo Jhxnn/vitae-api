6add0e62d0390b2d3ce4da7c7d2b578ebe701a0c
Hilixx <jhonalizan@gmail.com>
2025-07-19 12:02:08 -0300

    cv model

--- src/main/java/com/vitae_api/models/Cv.java
+++ src/main/java/com/vitae_api/models/Cv.java
@@ -1,4 +1,55 @@
 package com.vitae_api.models;
 
+import jakarta.persistence.*;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "cvs")
 public class Cv {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private UUID cvId;
+
+    @ManyToOne
+    @JoinColumn(referencedColumnName = "id", name = "user_id")
+    private User user;
+
+    private double grade;
+
+    private String content;
+
+    public UUID getCvId() {
+        return cvId;
+    }
+
+    public void setCvId(UUID cvId) {
+        this.cvId = cvId;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public double getGrade() {
+        return grade;
+    }
+
+    public void setGrade(double grade) {
+        this.grade = grade;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
 }
