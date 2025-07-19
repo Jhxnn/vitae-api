a926dcdb460e8f997aac6b0470ab10fafdaf169b
Hilixx <jhonalizan@gmail.com>
2025-07-19 11:52:23 -0300

    userModel

--- src/main/java/com/vitae_api/models/User.java
+++ src/main/java/com/vitae_api/models/User.java
@@ -1,4 +1,46 @@
 package com.vitae_api.models;
 
+
+import jakarta.persistence.*;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "users")
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private UUID userId;
+
+    private String name;
+
+    private String email;
+
+
+    public UUID getUserId() {
+        return userId;
+    }
+
+    public void setUserId(UUID userId) {
+        this.userId = userId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
+
