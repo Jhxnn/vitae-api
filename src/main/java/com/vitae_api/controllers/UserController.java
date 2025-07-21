59fa9059eb91aa7c8d9903d8995fd07f8a5bc35e
Hilixx <jhonalizan@gmail.com>
2025-07-20 21:55:30 -0300

    find all

--- src/main/java/com/vitae_api/controllers/UserController.java
+++ src/main/java/com/vitae_api/controllers/UserController.java
@@ -1,10 +1,16 @@
 package com.vitae_api.controllers;
 
 
+import com.vitae_api.models.User;
 import com.vitae_api.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/user")
@@ -14,6 +20,12 @@ public class UserController {
 
     @Autowired
     UserService userService;
+
+
+    @GetMapping
+    public ResponseEntity<List<User> findAll(){
+        return ResponseEntity.status(HttpStatus.OK).body(userService.findAll());
+    }
 
 
 }
