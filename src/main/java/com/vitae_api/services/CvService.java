ba081234e0ce40d7d6fb1c2c59254500d40b81f5
Hilixx <jhonalizan@gmail.com>
2025-07-21 15:25:49 -0300

    create CvDto

--- src/main/java/com/vitae_api/services/CvService.java
+++ src/main/java/com/vitae_api/services/CvService.java
@@ -1,4 +1,24 @@
 package com.vitae_api.services;
 
+
+import com.vitae_api.models.Cv;
+import com.vitae_api.repositories.CvRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class CvService {
+
+
+    @Autowired
+    UserService userService;
+
+    @Autowired
+    CvRepository cvRepository;
+
+
+    public Cv createCv(Cv cv){
+
+    }
+
 }
