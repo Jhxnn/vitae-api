79705812f0e9579ac2d48de1a83f4b873a62c142
Hilixx <jhonalizan@gmail.com>
2025-07-20 21:50:18 -0300

    teste branch

--- src/main/java/com/vitae_api/controllers/UserController.java
+++ src/main/java/com/vitae_api/controllers/UserController.java
@@ -1,4 +1,19 @@
 package com.vitae_api.controllers;
 
+
+import com.vitae_api.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/user")
 public class UserController {
+
+
+
+    @Autowired
+    UserService userService;
+
+
 }
