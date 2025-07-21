fffc516e20467cdf35119d8393eb101ee2fa4060
Hilixx <jhonalizan@gmail.com>
2025-07-21 15:28:47 -0300

    createCv functioN

--- src/main/java/com/vitae_api/services/CvService.java
+++ src/main/java/com/vitae_api/services/CvService.java
@@ -1,8 +1,11 @@
 package com.vitae_api.services;
 
 
+import com.vitae_api.dtos.CvDto;
 import com.vitae_api.models.Cv;
+import com.vitae_api.models.User;
 import com.vitae_api.repositories.CvRepository;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -17,8 +20,13 @@ public class CvService {
     CvRepository cvRepository;
 
 
-    public Cv createCv(Cv cv){
-
+    public Cv createCv(CvDto cvDto){
+        Cv cv = new Cv();
+        BeanUtils.copyProperties(cvDto, cv);
+        User user = userService.findById(cvDto.userId());
+        cv.setUser(user);
+        return cvRepository.save(cv);
     }
+
 
 }
