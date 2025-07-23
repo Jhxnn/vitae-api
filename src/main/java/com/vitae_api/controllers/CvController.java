9a5b98c688daf370f998c18be9c2dda4691414b4
Hilixx <jhonalizan@gmail.com>
2025-07-23 14:42:03 -0300

    cvService e cvController estrutura das funcoes

--- src/main/java/com/vitae_api/controllers/CvController.java
+++ src/main/java/com/vitae_api/controllers/CvController.java
@@ -1,4 +1,27 @@
 package com.vitae_api.controllers;
 
+
+import com.vitae_api.dtos.ChatResponseDto;
+import com.vitae_api.services.CvService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.query.Param;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+@RequestMapping("/api/v1/cv")
 public class CvController {
+
+
+    @Autowired
+    CvService cvService;
+
+    @PostMapping
+    public ResponseEntity<ChatResponseDto.ChoiceDto> cvResponse(@RequestParam("file") MultipartFile file){
+        return ResponseEntity.noContent().build();
+    }
 }
